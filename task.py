7757e819bcf57fe976b1e343b172d5fc01930d63
AsSimpson <23759@students.lynfield.school.nz>
2024-07-04 09:59:38 +1200

    Update task.py
    Version5(or)

--- task.py
+++ task.py
@@ -7,21 +7,22 @@ def quit():
 
 
 def generate_random():
-    bg_colour1 = "red"
-    bg_colour2 = "red"
+    bg_colour = "red"
+
     # random number 1
     random_number1 = random.randint(1, 10)
-    if random_number1 == 6:
-        bg_colour1 = "yellow"
-        print("We got 6 on left side. ")
-    Label(main_window, text=random_number1, bg=bg_colour1, width=11).grid(column=0, row=1, sticky=E)
-
     # random number2
     random_number2 = random.randint(1, 10)
-    if random_number2 == 6:
-        bg_colour2 = "yellow"
-        print("We got 6 on right side. ")
-    Label(main_window, text=random_number2, bg=bg_colour2, width=11).grid(column=1, row=1, sticky=E)
+
+    if random_number1 == 6 or random_number2 == 6:
+        bg_colour = "green"
+    # create labels
+    Label(main_window, text=random_number1, bg=bg_colour, width=10).grid(column=0, row=1, sticky=E)
+    Label(main_window, text=random_number2, bg=bg_colour, width=10).grid(column=1, row=1, sticky=E)
+
+    if random_number1 == 6 or random_number2 == 6:
+        bg_colour = "green"
+        print("We got 6. ")
 
 
 def main():
