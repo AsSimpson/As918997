6bf787e5972b4e2018fe6f69119967d753239b72
AsSimpson <23759@students.lynfield.school.nz>
2024-07-04 09:53:26 +1200

    Update task.v2.0.py

--- task.v1.0.py
+++ task.v1.0.py
@@ -7,26 +7,13 @@ def quit():
 
 
 def generate_random():
-    bg_colour1 = "red"
-    bg_colour2 = "red"
-    # random number 1
-    random_number1 = random.randint(1, 10)
-    if random_number1 == 6:
-        bg_colour1 = "yellow"
-        print("We got 6 on left side. ")
-    Label(main_window, text=random_number1, bg=bg_colour1, width=11).grid(column=0, row=1, sticky=E)
-
-    # random number2
-    random_number2 = random.randint(1, 10)
-    if random_number2 == 6:
-        bg_colour2 = "yellow"
-        print("We got 6 on right side. ")
-    Label(main_window, text=random_number2, bg=bg_colour2, width=11).grid(column=1, row=1, sticky=E)
+    random_number = random.randint(10, 20)
+    Label(main_window, text=random_number) .grid(column=0, row=2, sticky=E)
 
 
 def main():
-    Button(main_window, text="Quit", command=quit, width=10).grid(column=0, row=0)
-    Button(main_window, text="Random", command=generate_random, width=10).grid(column=1, row=0)
+    Button(main_window, text="Quit", command=quit, width=10)  .grid(column=0, row=0)
+    Button(main_window, text="Random", command=generate_random, width=10)  .grid(column=1, row=0)
     main_window.mainloop()
 
 
