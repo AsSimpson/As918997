b5ce80a13883acda88991d883281e1d7bc325ff0
Simpson <23759@studnets.lynfield.school.nz>
2024-07-31 12:24:16 +1200

    update practice.py

--- practice.py
+++ practice.py
@@ -3,21 +3,25 @@ window =tk.Tk()
 window.title('my window')
 window.geometry('200x200')
 
-e=tk.Entry(window,show=None)
-e.pack()
+var1=tk.StringVar()
+l=tk.Label(window,bg='yellow',width=4,textvariable=var1)
+l.pack()
 
-def insert_point():
-    var=e.get()
-    t.insert('insert',var)
-def insert_end():
-    var = e.get()
-    t.insert('end',var)
-
-b1=tk.Button(window,text='insert point',width=15,height=2,command=insert_point)
+def print_seletion():
+    value=lb.get(lb.curselection())
+    var1.set(value)
+b1=tk.Button(window,text='insert point',width=15,height=2,command=print_seletion)
 b1.pack()
-b2=tk.Button(window,text='insert end',command=insert_end)
-b2.pack()
-t=tk.Text(window,height=2)
-t.pack()
+
+var2=tk.StringVar()
+var2.set((11,4,5,14))
+lb=tk.Listbox(window,listvariable=var2)
+list_items=[1,2,3,4]
+for item in list_items:
+    lb.insert('end',item)
+lb.insert(1,'frist')
+lb.insert(2,'second')
+lb.delete(2)
+lb.pack()
 
 window.mainloop()
