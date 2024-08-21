7c41c443e054faab4f293011be3a9385805c566e
Simpson <23759@studnets.lynfield.school.nz>
2024-08-21 12:26:05 +1200

    fixed receipt header display error which happens after 'delete row'. Optimised the appearance of receipt text

--- main.py
+++ main.py
@@ -58,15 +58,15 @@ def append_item():
 def print_items_details():
     name_count = 0
     while name_count < counters['total_entries']:
-        Label(bottom, text=name_count + 1, relief="sunken", font=("Helvetica", 10), fg="blue", bg="IndianRed3").grid(
+        Label(bottom, text=name_count + 1, relief="sunken", font=("Helvetica", 10), fg="black", bg="IndianRed3").grid(
             column=0, row=name_count + 1, padx=5, pady=5)
-        Label(bottom, text=(Items_details[name_count][0]), relief="sunken", font=fontNum1, fg="blue",
+        Label(bottom, text=(Items_details[name_count][0]), relief="sunken", font=fontNum1, fg="black",
               bg="IndianRed3").grid(column=1, row=name_count + 1, padx=5, pady=5)
-        Label(bottom, text=(Items_details[name_count][1]), relief="sunken", font=("Helvetica", 10), fg="blue",
+        Label(bottom, text=(Items_details[name_count][1]), relief="sunken", font=fontNum1, fg="black",
               bg="IndianRed3").grid(column=2, row=name_count + 1, padx=5, pady=5)
-        Label(bottom, text=(Items_details[name_count][2]), relief="sunken", font=("Helvetica", 10), fg="blue",
+        Label(bottom, text=(Items_details[name_count][2]), relief="sunken", font=fontNum1, fg="black",
               bg="IndianRed3").grid(column=3, row=name_count + 1, padx=5, pady=5)
-        Label(bottom, text=(Items_details[name_count][3]), relief="sunken", font=("Helvetica", 10), fg="blue",
+        Label(bottom, text=(Items_details[name_count][3]), relief="sunken", font=fontNum1, fg="black",
               bg="IndianRed3").grid(column=4, row=name_count + 1, padx=5, pady=5)
         name_count += 1
         counters['name_count'] = name_count
@@ -92,9 +92,9 @@ def delete_row():
         name_count = counters['name_count']
         delete_item.delete(0, 'end')
         # Clear the last item displayed on the GUI
-        Label(bottom, text="            ", font=fontNum2, bg="IndianRed3").grid(column=0, row=name_count)
-        Label(bottom, text="            ", font=fontNum2, bg="IndianRed3").grid(column=1, row=name_count)
-        Label(bottom, text="            ", font=fontNum2, bg="IndianRed3").grid(column=2, row=name_count)
+        Label(bottom, text="    ", font=fontNum2, bg="IndianRed3").grid(column=0, row=name_count)
+        Label(bottom, text="                   ", font=fontNum2, bg="IndianRed3").grid(column=1, row=name_count)
+        Label(bottom, text="                 ", font=fontNum2, bg="IndianRed3").grid(column=2, row=name_count)
         Label(bottom, text="            ", font=fontNum2, bg="IndianRed3").grid(column=3, row=name_count)
         Label(bottom, text="            ", font=fontNum2, bg="IndianRed3").grid(column=4, row=name_count)
         # Print all the items in the list
